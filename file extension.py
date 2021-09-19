d4d797c1c18580b0e0fdb421b86c3381efa1f898
PushpaSharmaa <69301406+PushpaSharmaa@users.noreply.github.com>
2021-09-19 21:44:58 +0530

    file extension

---
+++ file extension.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 19 21:36:58 2021
+
+@author: UC17
+"""
+
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + f_extns[-1])
